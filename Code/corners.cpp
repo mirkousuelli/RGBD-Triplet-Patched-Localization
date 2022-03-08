64c056cb059095bd705c5300e11fbef98d4dcdca
Marco Petri <61703580+marcoPetriPolimi@users.noreply.github.com>
2022-03-08 17:22:00 +0100

    Gitignore fix and tabs instead of spaces

--- Code/corners.cpp
+++ Code/corners.cpp
@@ -6,35 +6,35 @@ using namespace cv;
 
 void CallBackFunc(int event, int x, int y, int flags, void* userdata)
 {
-    if  ( event == EVENT_LBUTTONDOWN || event == EVENT_RBUTTONDOWN || event == EVENT_MBUTTONDOWN )
-    {
-        cout << "Clicked - position (" << x << ", " << y << ")" << endl;
-    }
+	if  ( event == EVENT_LBUTTONDOWN || event == EVENT_RBUTTONDOWN || event == EVENT_MBUTTONDOWN )
+	{
+		cout << "Clicked - position (" << x << ", " << y << ")" << endl;
+	}
 }
 
 int main(int argc, char** argv)
 {
-    // Read image from file 
-    Mat img = imread("Dataset/00000-color.png");;
+	// Read image from file 
+	Mat img = imread("Dataset/00000-color.png");;
 
-    //if fail to read the image
-    if ( img.empty() ) 
-    { 
-        cout << "Error loading the image" << endl;
-        return -1; 
-    }
+	//if fail to read the image
+	if ( img.empty() ) 
+	{ 
+		cout << "Error loading the image" << endl;
+		return -1; 
+	}
 
-    //Create a window
-    namedWindow("My Window", 1);
+	//Create a window
+	namedWindow("My Window", 1);
 
-    //set the callback function for any mouse event
-    setMouseCallback("My Window", CallBackFunc, NULL);
+	//set the callback function for any mouse event
+	setMouseCallback("My Window", CallBackFunc, NULL);
 
-    //show the image
-    imshow("My Window", img);
+	//show the image
+	imshow("My Window", img);
 
-    // Wait until user press some key
-    waitKey(0);
+	// Wait until user press some key
+	waitKey(0);
 
-    return 0;
+	return 0;
 }
--- Code/point_cloud.cpp
+++ Code/point_cloud.cpp
@@ -25,61 +25,61 @@ pcl::visualization::PCLVisualizer::Ptr simpleVis (pcl::PointCloud<pcl::PointXYZR
 }
 
 int main() {
-    Mat rgb_image = imread("Dataset/00000-color.png");
-    Mat d_image = imread("Dataset/00000-depth.png");
+	Mat rgb_image = imread("Dataset/00000-color.png");
+	Mat d_image = imread("Dataset/00000-depth.png");
 
-    namedWindow("RGB Image", WINDOW_AUTOSIZE );
-    imshow("RGB Image", rgb_image);
-    namedWindow("Depth Image", WINDOW_AUTOSIZE );
-    imshow("Depth Image", d_image);
+	namedWindow("RGB Image", WINDOW_AUTOSIZE );
+	imshow("RGB Image", rgb_image);
+	namedWindow("Depth Image", WINDOW_AUTOSIZE );
+	imshow("Depth Image", d_image);
 
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>); 
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>); 
 
-    // my camera parameters
-    float cx = 67.0f; //optical center x coordinate
-    float cy = 1028.7f; //optical center y coordinate
-    float f = 512.5f; //focal length (the same for x and y)
+	// my camera parameters
+	float cx = 67.0f; //optical center x coordinate
+	float cy = 1028.7f; //optical center y coordinate
+	float f = 512.5f; //focal length (the same for x and y)
 
-    // default parameters
-    //float cx = 319.5f; //optical center x coordinate
-    //float cy = 239.5f; //optical center y coordinate
-    //float f = 525.0f; //focal length (the same for x and y)
+	// default parameters
+	//float cx = 319.5f; //optical center x coordinate
+	//float cy = 239.5f; //optical center y coordinate
+	//float f = 525.0f; //focal length (the same for x and y)
 
-    pcl::PointXYZRGB point;
+	pcl::PointXYZRGB point;
 
-    cout << rgb_image.cols << endl;
-    cout << rgb_image.rows << endl;
-    int prod = rgb_image.cols * rgb_image.rows;
+	cout << rgb_image.cols << endl;
+	cout << rgb_image.rows << endl;
+	int prod = rgb_image.cols * rgb_image.rows;
 
-    int imageDepth;
-    cv::Vec3b pixel;
-    int i = 0;
-    for (int imageWidth = 0; imageWidth < rgb_image.cols - 100; imageWidth++) {
-        for (int imageHeight = 0; imageHeight < rgb_image.rows - 200; imageHeight++) {
-            imageDepth = d_image.at<int>(imageWidth, imageHeight);
-            point.x = (cx - imageWidth) / f;
-            point.y = (cy - imageHeight) / f;
-            point.z = imageDepth / sqrt(1 + pow(point.x, 2)+ pow(point.y, 2));
-            point.x *= point.z;
-            point.y *= point.z;
-            pixel = rgb_image.at<cv::Vec3b>(imageWidth, imageHeight);
-            point.r = pixel[2];
-            point.g = pixel[1];
-            point.b = pixel[0];
-            cloud->points.push_back(point);
-            cout << ++i << " / " << prod << endl ;
-        }
-    }
+	int imageDepth;
+	cv::Vec3b pixel;
+	int i = 0;
+	for (int imageWidth = 0; imageWidth < rgb_image.cols - 100; imageWidth++) {
+		for (int imageHeight = 0; imageHeight < rgb_image.rows - 200; imageHeight++) {
+			imageDepth = d_image.at<int>(imageWidth, imageHeight);
+			point.x = (cx - imageWidth) / f;
+			point.y = (cy - imageHeight) / f;
+			point.z = imageDepth / sqrt(1 + pow(point.x, 2)+ pow(point.y, 2));
+			point.x *= point.z;
+			point.y *= point.z;
+			pixel = rgb_image.at<cv::Vec3b>(imageWidth, imageHeight);
+			point.r = pixel[2];
+			point.g = pixel[1];
+			point.b = pixel[0];
+			cloud->points.push_back(point);
+			cout << ++i << " / " << prod << endl ;
+		}
+	}
 
-    pcl::visualization::PCLVisualizer::Ptr viewer;
-    viewer = simpleVis(cloud);
+	pcl::visualization::PCLVisualizer::Ptr viewer;
+	viewer = simpleVis(cloud);
 
-    while (!viewer->wasStopped ())
-    {
-        viewer->spinOnce (100);
-    }
-    
-    waitKey(0);
+	while (!viewer->wasStopped ())
+	{
+		viewer->spinOnce (100);
+	}
+	
+	waitKey(0);
 
 	return 0;
 }
