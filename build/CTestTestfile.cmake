acbc90db228cd2f030034eae37b3d0bbfce46586
Marco Petri <61703580+marcoPetriPolimi@users.noreply.github.com>
2022-03-08 16:50:12 +0100

    removed files to be ignored

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mirkousuelli/OneDrive - Politecnico di Milano/GitHub/rgb-d-semantic-sampling
-# Build directory: /Users/mirkousuelli/OneDrive - Politecnico di Milano/GitHub/rgb-d-semantic-sampling/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
