47e3969b2eee15434e99141b8fc420605cbda1d0
Mirko Usuelli <mirko.usuelli@mail.polimi.it>
2022-03-10 17:20:57 +0100

    point cloud dataset png fixed

--- Code/prova.py
+++ Code/prova.py
@@ -2,14 +2,14 @@
 import open3d as o3d
 from matplotlib import pyplot as plt
 
-redwood_rgbd = o3d.data.SampleRedwoodRGBDImages()
-color_raw = o3d.io.read_image(redwood_rgbd.color_paths[0])
-depth_raw = o3d.io.read_image(redwood_rgbd.depth_paths[0])
-rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
+#redwood_rgbd = o3d.data.SampleRedwoodRGBDImages()
+color_raw = o3d.io.read_image('../Dataset/00000-color.png')#)redwood_rgbd.color_paths[0])
+depth_raw = o3d.io.read_image('../Dataset/00000-depth.png')#)redwood_rgbd.depth_paths[0])
+rgbd_image = o3d.geometry.RGBDImage.create_from_tum_format(color_raw, depth_raw)
 print(rgbd_image)
 
 plt.subplot(1, 2, 1)
-plt.title('Grayscale image')
+plt.title('RGB image')
 plt.imshow(rgbd_image.color)
 plt.subplot(1, 2, 2)
 plt.title('Depth image')
@@ -20,10 +20,11 @@ pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
     rgbd_image,
     o3d.camera.PinholeCameraIntrinsic(
         o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
+
 # Flip it, otherwise the point cloud will be upside down
 pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
 vis = o3d.visualization.Visualizer()
 vis.create_window()
 vis.add_geometry(pcd)
-o3d.visualization.ViewControl.set_zoom(vis.get_view_control(), 0.8)
+o3d.visualization.ViewControl.set_zoom(vis.get_view_control(), 0.25)
 vis.run()
