52a3b5ad134fd27897ce750de15b126af4397dd4
Mirko Usuelli <mirko.usuelli@mail.polimi.it>
2022-03-14 23:59:18 +0100

    Merger added (still to be done)

---
+++ Code/tools/Merger.py
@@ -0,0 +1,10 @@
+"""
+Project : RGB-D Semantic Sampling
+Authors : Marco Petri and Mirko Usuelli
+-----------------------------------------------
+Degree : M.Sc. Computer Science and Engineering
+Course : Image Analysis and Computer Vision
+Professor : Vincenzo Caglioti
+Advisors : Giacomo Boracchi, Luca Magri
+University : Politecnico di Milano - (A.Y. 2021/2022)
+"""
