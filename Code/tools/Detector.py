4494a8f4e6145a67b5e7abbe65754441ddd384f1
Mirko Usuelli <mirko.usuelli@mail.polimi.it>
2022-03-14 23:16:51 +0100

    Detector computation done

--- Code/tools/Detector.py
+++ Code/tools/Detector.py
@@ -1,31 +1,66 @@
+"""
+Project : RGB-D Semantic Sampling
+Authors : Marco Petri and Mirko Usuelli
+-----------------------------------------------
+Degree : M.Sc. Computer Science and Engineering
+Course : Image Analysis and Computer Vision
+Professor : Vincenzo Caglioti
+Advisors : Giacomo Boracchi, Luca Magri
+University : Politecnico di Milano
+A.Y. : 2021 - 2022
+"""
 import cv2
 
 
 class Detector:
+    """
+    Class implementing the tool 'Detector' able to detect relevant
+    corners/features in a single image.
+    """
+
+    # Techniques available:
     ORB = 'ORB'
     SIFT = 'SIFT'
     DNN = 'DNN'
 
     def __init__(self, _num_features, _method):
+        """
+        Constructor.
+        I : number of features to be detected and the desired detecting method
+        O : -
+        """
         self.num_features = _num_features
 
+        # method choice
         if _method == self.ORB:
             self.core = cv2.ORB_create(self.num_features)
-            print('[Detector] ORB detector initialized')
         elif _method == self.SIFT:
             self.core = cv2.SIFT_create(self.num_features)
-            print('[Detector] SIFT detector initialized')
         elif _method == self.DNN:
-            print('[Detector][WIP] DNN to be done yet...')
+            print('\033[91m' + '[Detector][WIP] DNN to be done yet...' + '\033[0m')
         else:
             print('\033[91m' + '[Detector][ERROR] Method not found' + '\033[0m')
 
     @staticmethod
     def __preprocess(_img):
+        """
+        Private static method useful to preprocess the image in grayscale and
+        in a built-in way within the class.
+        I : image in RGB
+        O : image in grayscale
+        """
+        # checking that the image has more than one channel
         assert _img.shape[2] > 1
-        print('[Detector] Image preprocessed to gray scale')
+
+        # returning it back to grayscale
         return cv2.cvtColor(_img, cv2.COLOR_BGR2GRAY)
 
     def detectAndCompute(self, _img):
-        print('[Detector] Key points and descriptors are going to be detected')
+        """
+        Merge the behaviour of all possible core techniques in one function in purpose
+        of detecting and computing features.
+        I : image
+        O : key points within the given image + their associated descriptors
+        """
+        # before the proper detection it is needed a grayscale preprocess
         return self.core.detectAndCompute(self.__preprocess(_img), None)
--- Code/tools/Matcher.py
+++ Code/tools/Matcher.py
@@ -1,3 +1,14 @@
+"""
+Project : RGB-D Semantic Sampling
+Authors : Marco Petri and Mirko Usuelli
+-----------------------------------------------
+Degree : M.Sc. Computer Science and Engineering
+Course : Image Analysis and Computer Vision
+Professor : Vincenzo Caglioti
+Advisors : Giacomo Boracchi, Luca Magri
+University : Politecnico di Milano
+A.Y. : 2021 - 2022
+"""
 import cv2
 
 
@@ -12,9 +23,8 @@ class Matcher:
             index_params = dict(algorithm=6, table_number=6, key_size=12, multi_probe_level=1)
             search_params = dict(checks=self.num_features)
             self.core = cv2.FlannBasedMatcher(indexParams=index_params, searchParams=search_params)
-            print('[Matcher] Flann matcher initialized')
         elif _method == self.DNN:
-            print('[Matcher][WIP] DNN matcher to be done yet...')
+            print('\033[91m' + '[Matcher][WIP] DNN matcher to be done yet...' + '\033[0m')
         else:
             print('\033[91m' + '[Matcher][ERROR] Method not found' + '\033[0m')
 
@@ -24,7 +34,6 @@ class Matcher:
         try:
             for m, n in _matches:
                 good.append(m if m.distance < n.distance else n)
-            print('[Matcher] Matches successfully filtered')
             good.sort(key=lambda x: x.distance, reverse=False)
             return good
         except ValueError:
@@ -34,7 +43,6 @@ class Matcher:
     def match(self, _descriptors_1, _descriptors_2):
         matches = self.core.knnMatch(_descriptors_1, _descriptors_2, k=2)
         matches = [x for x in matches if len(x) == 2]
-        print('[Matcher] Matches computed')
         return self.__filter(matches)
 
     @staticmethod
@@ -47,5 +55,4 @@ class Matcher:
         final_img = cv2.drawMatches(_img_1, _key_points_1,
                                     _img_2, _key_points_2,
                                     _matches[:limit], None, **draw_params)
-        print('[Matcher] Matches ready to be drawn')
         return cv2.resize(final_img, (_img_1.shape[1] * 2, _img_1.shape[0]))
