dfa7e536dd0a02b1154928314e0567010296f34d
Mirko Usuelli <mirko.usuelli@mail.polimi.it>
2022-03-14 22:43:28 +0100

    Testing scripts renamed

--- Code/tools/Matcher.py
+++ Code/tools/Matcher.py
@@ -5,9 +5,8 @@ class Matcher:
     FLANN = 'FLANN'
     DNN = 'DNN'
 
-    def __init__(self, _num_features, _method, _accuracy):
+    def __init__(self, _num_features, _method):
         self.num_features = _num_features
-        self.accuracy = _accuracy
 
         if _method == self.FLANN:
             index_params = dict(algorithm=6, table_number=6, key_size=12, multi_probe_level=1)
@@ -20,21 +19,26 @@ class Matcher:
             print('\033[91m' + '[Matcher][ERROR] Method not found' + '\033[0m')
 
     @staticmethod
-    def __filter(_accuracy, _matches):
+    def __filter(_matches):
         good = []
-        for m, n in _matches:
-            if m.distance < _accuracy * n.distance:
-                good.append(m)
-        print('[Matcher] Matches successfully filtered')
-        return good
+        try:
+            for m, n in _matches:
+                good.append(m if m.distance < n.distance else n)
+            print('[Matcher] Matches successfully filtered')
+            good.sort(key=lambda x: x.distance, reverse=False)
+            return good
+        except ValueError:
+            print('\033[91m' + '[Matcher][ERROR] kNN matching error' + '\033[0m')
+            pass
 
     def match(self, _descriptors_1, _descriptors_2):
         matches = self.core.knnMatch(_descriptors_1, _descriptors_2, k=2)
+        matches = [x for x in matches if len(x) == 2]
         print('[Matcher] Matches computed')
-        return self.__filter(self.accuracy, matches)
+        return self.__filter(matches)
 
     @staticmethod
-    def drawMatches(_img_1, _key_points_1, _img_2, _key_points_2, _matches):
+    def drawMatches(_img_1, _key_points_1, _img_2, _key_points_2, _matches, limit=-1):
         assert _img_1.shape == _img_2.shape
         draw_params = dict(matchColor=-1,  # draw matches in green color
                            singlePointColor=None,
@@ -42,6 +46,6 @@ class Matcher:
                            flags=2)
         final_img = cv2.drawMatches(_img_1, _key_points_1,
                                     _img_2, _key_points_2,
-                                    _matches, None, **draw_params)
+                                    _matches[:limit], None, **draw_params)
         print('[Matcher] Matches ready to be drawn')
         return cv2.resize(final_img, (_img_1.shape[1] * 2, _img_1.shape[0]))
