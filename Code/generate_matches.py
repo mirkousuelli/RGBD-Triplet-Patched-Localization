Minor fixes and quaternions file creator

Code/generate_matches.py
@@ -1,19 +1,40 @@
+import numpy as np
+
 from camera.Action import Action
 from camera.Frame import Frame
 from tools.Localizer import Localizer
 from tools.Merger import Merger
 from utils.utils import get_str
 
+def multiply_quaternions(q1: np.ndarray, q2: np.ndarray) -> np.ndarray:
+	"""Gets to quaternions and multiply them.
+	
+	:param q1: First quaternion.
+	:param q2: Second quaternion.
+	:return: Resulting quaternion.
+	
+	Notes
+	-----
+	See https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation for more
+	details on quaternion multiplication.
+	"""
+	new_a = q1[0]*q2[0] - q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3]
+	new_b = q1[0]*q2[1] + q1[1]*q2[0] + q1[2]*q2[3] - q1[3]*q2[2]
+	new_c = q1[0]*q2[2] + q1[2]*q2[0] + q1[3]*q2[1] - q1[1]*q2[3]
+	new_d = q1[0]*q2[3] + q1[3]*q2[0] + q1[1]*q2[2] - q1[2]*q2[1]
+	return np.array([new_a, new_b, new_c, new_d])
+
 verbose = True
-where_to_write = "localization/01.matches"
+where_to_write = "localization/01_direct.matches"
 matches_str = "1 0 0 0 0 0 0"
 for i in range(1, 834, 1):
-	action = Action(Frame("../Dataset/Colors/00000-color.png",
-						  "../Dataset/Depths/00000-depth.png",
-						  0),
-					Frame("../Dataset/Colors/00" + get_str(i) + "-color.png",
-						  "../Dataset/Depths/00" + get_str(i) + "-depth.png",
-						  i))
+	first_img = Frame("../Dataset/Colors/00" + get_str(i-1) + "-color.png",
+					  "../Dataset/Depths/00" + get_str(i-1) + "-depth.png",
+					  i-1)
+	second_img = Frame("../Dataset/Colors/00" + get_str(i) + "-color.png",
+					   "../Dataset/Depths/00" + get_str(i) + "-depth.png",
+					   i)
+	action = Action(first_img, second_img)
 	
 	if verbose:
 		print("Executing step: ", i)
@@ -23,11 +44,49 @@ for i in range(1, 834, 1):
 					matcher_method="FLANN")
 	merge_image = merger.merge_action(action)
 	
-	Localizer.roto_translation(action, normalize_em=False)
-	q = Localizer.from_rot_to_quat(action, normalize_em=False)
-	matches_str += "\n" + str(q[0]) + str(q[1]) + str(q[2]) + str(q[3])
-	matches_str += str(action.t[0]) + str(action.t[1]) + str(action.t[2])
+	localizer = Localizer()
+	localizer.compute_fundamental_matrix(action)
+	localizer.compute_essential_matrix(action)
+	localizer.roto_translation(action, normalize_em=False)
+	q = localizer.from_rot_to_quat(action, normalize_em=False)
+	matches_str += "\n" + str(q[0]) + " " + str(q[1]) + " " + str(q[2]) + " " + str(q[3])
+	matches_str += " " + str(action.t[0]) + " " + str(action.t[1]) + " " + str(action.t[2])
+	print("Figure ", action.first._Frame__color_path, " and figure ", action.second._Frame__color_path)
+	print(str(q[0]) + " " + str(q[1]) + " " + str(q[2]) + " " + str(q[3]) + " " + str(action.t[0]) + " " + str(action.t[1]) + " " + str(action.t[2]))
+	del action
+	del first_img
+	del second_img
+	del q
+	del merge_image
+	del merger
+	del localizer
+
+clean_matches = "1 0 0 0 0 0 0"
+
+overall_quaternion = np.array([1, 0, 0, 0])
+overall_translation = np.array([0, 0, 0])
+lines = matches_str.split("\n")
+for i in range(1, 834, 1):
+	break
+	relative_pos = lines[i].split(" ")
+	relative_pos = np.array(relative_pos, dtype=float)
 	
-f = open(where_to_write)
-f.write(matches_str)
-f.close()
+	quaternion = relative_pos[0:4]
+	position = relative_pos[4:7]
+	
+	if verbose:
+		print("Getting results step: ", i)
+	
+	overall_quaternion = multiply_quaternions(overall_quaternion, quaternion)
+	overall_translation = overall_translation + position
+	
+	q = overall_quaternion
+	t = overall_translation
+	
+	clean_matches += "\n" + str(q[0]) + " " + str(q[1]) + " " + str(q[2]) + " " + str(q[3])
+	clean_matches += " " + str(t[0]) + " " + str(t[1]) + " " + str(t[2])
+
+#f = open(where_to_write, "w")
+#f.write(matches_str)
+#f.close()
+

Code/tools/Localizer.py
@@ -244,7 +244,7 @@ class Localizer:
 	def roto_translation(
 		action: Action,
 		inplace=True,
-		normalize_em=True
+		normalize_em=False
 	) -> Union[None, Tuple[np.mat, np.ndarray]]:
 		"""
 		Compute the roto-translation components, i.e. the rotation matrix and
@@ -260,7 +260,7 @@ class Localizer:
 		
 		:param normalize_em:
 			States whether the essential matrix must be normalized or not to fit
-			the roto-translation.
+			the roto-translation. It can be used only with inplace = True.
 		:type normalize_em:
 
 		:return:
@@ -269,13 +269,16 @@ class Localizer:
 		"""
 		# pre-conditions
 		if action.e_matrix is None:
-			Localizer.compute_essential_matrix(action, inplace)
+			if inplace:
+				Localizer.compute_essential_matrix(action, inplace)
+			else:
+				e_matrix = Localizer.compute_essential_matrix(action, inplace)
 
-		if normalize_em:
+		if normalize_em and inplace:
 			action.normalize_essential_matrix()
 		
 		# SVD decomposition of the essential matrix
-		w, u, vt = cv2.SVDecomp(action.e_matrix)
+		w, u, vt = cv2.SVDecomp(action.e_matrix if inplace else e_matrix)
 
 		# determinant adjustments
 		if np.linalg.det(u) < 0:
@@ -287,7 +290,7 @@ class Localizer:
 		            [1, 0, 0],
 		            [0, 0, 1]], dtype=float)
 		
-		if normalize_em:
+		if normalize_em and inplace:
 			Localizer.compute_essential_matrix(action, inplace)
 
 		# return the roto-translation components

Code/tools/Visualizer.py
@@ -130,7 +130,7 @@ class Visualizer(ProjectObject):
 		# Transform the image using the pose of the camera
 		frame_pose = frame.extract_pose()
 		quaternions = frame_pose[0:4]
-		position = frame_pose[4:7] * np.array([0.3, 0.5, 0.5])
+		position = frame_pose[4:7]
 		rotation = PointCloud.get_rotation_matrix_from_quaternion(quaternions)
 		
 		result = pcd